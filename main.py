cd252384d85db18415acc942680dc02b8525e22c
Darwinxkk <ryanalves140307@gmail.com>
2026-03-03 17:53:23 -0300

    Adição Automatica de Cargos dos Membros no Servidor e Recado de Saida do Membro

--- main.py
+++ main.py
@@ -21,22 +21,38 @@ class SyncBot(commands.Bot):
 
 bot = SyncBot()
 
+# Comando de Slash
+
 @bot.tree.command(name="ola_mundo", description="Ola Querido Mundo!")
 async def olamundo(interaction: discord.Interaction):
     await interaction.response.send_message(
         f"Ola {interaction.user.mention}!"
     )
 
-# Evento de novo membro
+# Novo membro
 @bot.event
 async def on_member_join(member: discord.Member):
     canal_bemvindo = bot.get_channel(1477821288436334644)
     canal_regras = bot.get_channel(1477396107339370576)
+    cargo = member.guild.get_role(1477447780979970058)
+
+    if cargo:
+        await member.add_roles(cargo)
 
     if canal_bemvindo and canal_regras:
         await canal_bemvindo.send(
             f"Bem Vindo {member.mention} ao Servidor, "
             f"Leia as regras {canal_regras.mention}!"
+        )
+
+#Saida de Membro
+@bot.event
+async def on_member_remove(member: discord.Member):
+    canal_bemvindo = bot.get_channel(1477821288436334644)
+
+    if canal_bemvindo:
+        await canal_bemvindo.send(
+            f"{member.mention} saiu do servidor!"
         )
 
 # Token correta via variável de ambiente
