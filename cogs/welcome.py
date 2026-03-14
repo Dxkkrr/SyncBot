818c623f04596d3c2804d90820ff434fa8ea1c2a
Darwinxkk <ryanalves140307@gmail.com>
2026-03-14 16:24:14 -0300

    Patch 1.5 - Otimização e Organização de Códigos

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -10,18 +10,21 @@ class Welcome(commands.Cog):
 # Entrada de Membro no Servidor
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        global entradas
+        entradas += 1
+        
+        canal_bemvindo = self.bot.get_channel(1482274529098076250) #ID Bem-Vindos
+        canal_regras = self.bot.get_channel(1477396107339370576) #ID Regras
 
-        canal_bemvindo = self.bot.get_channel(1477821288436334644)
-        canal_regras = self.bot.get_channel(1477396107339370576)
-
-        cargo = member.guild.get_role(1477447780979970058)
+        cargo = member.guild.get_role(1477447780979970058) #ID cargo Membro
 
         if cargo:
             await member.add_roles(cargo)
 
         if canal_bemvindo and canal_regras:
-            await canal_bemvindo.send(f"Bem Vindo {member.mention} ao Servidor!\n"
-                                    f"Leia as regras no canal {canal_regras.mention} para evitar punições!")
+            await canal_bemvindo.send(f"👋 Bem Vindo {member.mention} ao Servidor! 👋\n"
+                                    f"📖 Leia as regras no canal {canal_regras.mention} para evitar punições! 📖\n"
+                                    f"👥 Atualmente temos **{member.guild.member_count}** de Membros no servidor 👥")
         
         await atualizar_painel(self.bot, member.guild)
         
@@ -30,7 +33,7 @@ class Welcome(commands.Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         
-        canal_bemvindo = self.bot.get_channel(1477821288436334644)
+        canal_bemvindo = self.bot.get_channel(1482450659977592852) #ID Saídas
         if canal_bemvindo:
             await canal_bemvindo.send(f"{member.mention} saiu do servidor.")
             
